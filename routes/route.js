54e56628b575258fc27042ed30d10de6865f80ae
4molybdenum2 <tathagatapaul7@gmail.com>
2020-05-08 22:09:44 +0530

    major changes

--- routes/route.js
+++ routes/route.js
@@ -6,6 +6,7 @@ const db = require('../db/db');
 router.get('/',(req,res)=>{
     res.render('index');
 })
+
 router.get('/student',(req,res)=>{
     let sql = 'SELECT * FROM student';
     let query = db.query(sql,(err,result)=>{
@@ -13,6 +14,7 @@ router.get('/student',(req,res)=>{
         res.render('student' ,{title:"Student Table" , studentData: result});
     })
 })
+
 router.get('/admission',(req,res)=>{
     let sql = 'SELECT * FROM admission';
     let query = db.query(sql,(err,result)=>{
@@ -20,6 +22,7 @@ router.get('/admission',(req,res)=>{
         res.render('admission' ,{title:"Admission Table" , admissionData: result});
     })
 })
+
 router.get('/batch',(req,res)=>{
     let sql = 'SELECT * FROM batch';
     let query = db.query(sql,(err,result)=>{
@@ -27,6 +30,7 @@ router.get('/batch',(req,res)=>{
         res.render('batch' ,{title:"Batch Table" , batchData: result});
     })
 })
+
 router.get('/branch',(req,res)=>{
     let sql = 'SELECT * FROM branch_info';
     let query = db.query(sql,(err,result)=>{
@@ -34,4 +38,37 @@ router.get('/branch',(req,res)=>{
         res.render('branch' ,{title:"Branch Table" , branchData: result});
     })
 })
+
+router.get('/lecturer',(req,res)=>{
+    let sql = 'SELECT * FROM lecturer';
+    let query = db.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.render('lecturer' ,{title:"Lecturer Table" , lecturerData: result});
+    })
+})
+
+router.get('/lectures',(req,res)=>{
+    let sql = 'SELECT * FROM lectures';
+    let query = db.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.render('lectures' ,{title:"Lectures Table" , lecturesData: result});
+    })
+})
+
+router.get('/subject',(req,res)=>{
+    let sql = 'SELECT * FROM subject';
+    let query = db.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.render('subject' ,{title:"Subject Table" , subjectData: result});
+    })
+})
+
+router.get('/timetable',(req,res)=>{
+    let sql = 'SELECT * FROM timetable';
+    let query = db.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.render('timetable' ,{title:"Timetable Info" , timetableData: result});
+    })
+})
+
 module.exports = router;
